98c1dd7c0bde3b6953ada6fe0e21c23bd0283f81
CarlosRBF <carlosrblanco00@gmail.com>
2023-01-30 02:06:15 -0300

    feat: add documentation

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -28,12 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    "https://desafiobackendpython-production.up.railway.app/",
-    "0.0.0.0",
-]
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
@@ -182,3 +179,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
